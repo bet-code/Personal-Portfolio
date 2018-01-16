93127a37ec77a064c63808e447c83cf9d05ba919
Gilbert <gilbert.j5@outlook.com>
2018-01-16 14:52:35 -0500

    add and edit assets, styles and markup

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,28 @@
 
 portfolio = {}
 
+portfolio.smoothScroll = () => {
+    $('a').smoothScroll();
+}
+
 portfolio.form = () => {
     const contactForm = $('#contactForm');
     contactForm.attr('action', `https://formspree.io/code@gilbertjose.ca`)
 }
 
+portfolio.skillShow =  setInterval(() => {
+    const randNum = Math.floor(Math.random() * $('.skillList li').length +1);
+    let $skillShowItem = $('.skillList li').eq(randNum);    
+        $skillShowItem.addClass('showIcon')     
+        .siblings().removeClass('showIcon');
+        $skillShowItem.find('i').addClass('iconColour')
+        .parent().siblings().find('i').removeClass('iconColour');
+}, 1500);
+
 portfolio.init = () => {
+    portfolio.smoothScroll();
     portfolio.form();
+    portfolio.skillShow;
 }
 
 $(() => portfolio.init());
